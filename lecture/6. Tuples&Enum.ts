0c55193335c831d505c7f8d79164fd03ecb06e16
slowteady <vbn0213@gmail.com>
2023-03-25 23:07:18 +0900

    튜플, 이넘 학습 완료

---
+++ lecture/6. Tuples&Enum.ts
@@ -0,0 +1,15 @@
+// 1. 튜플 타입을 사용하여 배열의 길이나, 요소의 갯수 등을 지정할 수 있다.
+const tuples: [number, string] = [123, "asd"];
+
+ // 2. Enum을 사용하여 상수 집합을 만들 수 있다.
+ enum Responese {
+  A,
+  B,
+  C
+ }
+
+ // 3. Enum의 값을 지정할 수 있다
+ enum Res {
+  A = "asd",
+  B = 123,
+ }
