9a4b523166f059473ff6508267d1254032b6cb92
slowteady <vbn0213@gmail.com>
2023-03-26 23:19:15 +0900

    타입 단언 학습 완료

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("HELLO!!!!!!!!!!!!!");
+const btn = document.getElementById("btn");
+const input  = document.getElementById("todoinput")! as HTMLInputElement;
+
+btn?.addEventListener("click", function() {
+  alert(input.value);
+  input.value = "";
+});
+
+
+
