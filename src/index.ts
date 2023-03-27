42ee987d7e36817aec5a1311d19b159976e0d67e
LeeYongMin01 <vbn0213@gmail.com>
2023-03-27 12:29:43 +0900

    미니 프로젝트 진행중

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,39 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+const todos: Todo[] = [];
+
 const btn = document.getElementById("btn");
-const input  = document.getElementById("todoinput")! as HTMLInputElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
 
-btn?.addEventListener("click", function() {
-  alert(input.value);
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+
   input.value = "";
-});
+}
 
+function createTodo(todo: Todo) {
+  const newLI = document.createElement("LI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
 
+  newLI.append(todo.text );
+  newLI.append(checkbox);
+  list.append(newLI);
+}
 
+form.addEventListener("submit", handleSubmit);
+
+// btn.addEventListener("click", function() {
+//   alert(input.value);
+//   input.value = "";
+// });
