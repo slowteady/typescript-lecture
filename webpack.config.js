e1b45079f2456b50daf192ddd1b788bdd51f9f46
slowteady <vbn0213@gmail.com>
2023-04-02 16:58:33 +0900

    clean-webpack-plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
+  // mode: 개발, 운영 모드를 설정
+  mode: "development",
   // entry: 시작점
   entry: "./src/index.ts",
+  // devtool: 개발 환경에서의 필요한 3rd party 
+  devtool: "inline-source-map",
   // rule: 규칙 설정
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         // use: 사용할 3rd party
-        use: 'ts-loader',
+        use: "ts-loader",
         // exclude: 제외대상
         exclude: /node_modules/,
       },
@@ -16,10 +20,11 @@ module.exports = {
   },
   resolve: {
     // extensions: resolve할 파일의 확장자
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist'),
-  }
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+  },
 };
